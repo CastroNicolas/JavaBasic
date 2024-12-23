030558d217e8b0e16aa9c0c0bddedf9741f225e2
CastroNicolas <maximonicolascastro@gmail.com>
2024-12-23 15:59:34 +0100

    Boolean, annd NumberSitem and scanner

--- src/PrimitiveBoolean.java
+++ src/PrimitiveBoolean.java
@@ -1,4 +1,4 @@
-public class PrimitiveBoolean {
+public class n PrimitiveBoolean {
     public static void main(String[] args) {
          boolean datalogic = true; // or false ---> < > ! == ===
         System.out.println("datalogic = " + datalogic);
